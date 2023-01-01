f357b7db4c09c5b9afc54683f3cec58a9680b2be
ElyorxonB <117801522+ElyorxonB@users.noreply.github.com>
2023-01-01 09:36:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var time = +prompt('Введите Время');
+
+if(time === 0) {
+    console.log((+time) + ' 12 часов ночи');
+    alert('12 часов ночи')
+}else if(time === 1) {
+    console.log((+time) + ' 1 час ночи');
+    alert('1 час ночи')
+}else if(time === 2) {
+    console.log((+time) + ' 2 часа ночи');
+    alert('2 часов ночи')
+}else if(time === 3) {
+    console.log((+time) + ' 3 часа ночи');
+    alert('3 часов ночи')
+}else if(time === 4) {
+    console.log((+time) + ' 4 часа ночи');
+    alert('4 часов ночи')
+}else if(time === 5) {
+    console.log((+time) + ' 5 часов ночи');
+    alert('5 часов ночи')
+}else if(time === 6) {
+    console.log((+time) + ' 6 часов ночи');
+    alert('6 часов ночи')
+}else if(time === 7) {
+    console.log((+time) + ' 7 часов утра');
+    alert('7 часов утра')
+}else if(time === 8) {
+    console.log((+time) + ' 8 часов утра');
+    alert('8 часов утра')
+}else if(time === 9) {
+    console.log((+time) + ' 9 часов утра');
+    alert('9 часов утра')
+}else if(time === 10) {
+    console.log((+time) + ' 10 часов утра');
+    alert('10 часов утра')
+}else if(time === 11) {
+    console.log((+time) + ' 11 часов дня');
+    alert('11 часов дня')
+}else if(time === 12) {
+    console.log((+time) + ' 12 часов дня');
+    alert('12 часов дня')
+}else if(time === 13) {
+    console.log((+time) + ' 1 час дня');
+    alert('1 час дня')
+}else if(time === 14) {
+    console.log((+time) + ' 2 часа дня');
+    alert('2 часа дня')
+}else if(time === 15) {
+    console.log((+time) + ' 3 часа дня');
+    alert('3 часа дня')
+}else if(time === 16) {
+    console.log((+time) + ' 4 часа дня');
+    alert('4 часа дня')
+}else if(time === 17) {
+    console.log((+time) + ' 5 часов дня');
+    alert('5 часов дня')
+}else if(time === 18) {
+    console.log((+time) + ' 6 часов вечера');
+    alert('6 часов вечера')
+}else if(time === 19) {
+    console.log((+time) + ' 7 часов вечера');
+    alert('7 часов вечера')
+}else if(time === 20) {
+    console.log((+time) + ' 8 часов вечера');
+    alert('8 часов вечера')
+}else if(time === 21) {
+    console.log((+time) + ' 9 часов вечера');
+    alert('9 часов вечера')
+}else if(time === 22) {
+    console.log((+time) + ' 10 часов вечера');
+    alert('10 часов вечера')
+}else if(time === 23) {
+    console.log((+time) + ' 11 часов вечера');
+    alert('11 часов вечера')
+}else {
+    console.log('wrong');
+    alert('Введите от 0-24')
+}
